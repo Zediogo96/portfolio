7cb231246bcf236b99be05effbd1de7a861ba597
José Diogo <68304439+Zediogo96@users.noreply.github.com>
2023-07-13 22:07:30 +0100

    Updates

---
+++ assets/data/projects.tsx
@@ -0,0 +1,29 @@
+
+const ProjectsData = [
+	{
+		imgsrc: "projects/proj1.jpg", // "pro1
+		title: "File Transfer Protocol (FTP)",
+		text: "Allows transfer of files between to PC connetected to a wired network.",
+		github_url: "https://github.com/Zediogo96/RCOM_proj1",
+	},
+	{
+		imgsrc: "projects/proj2.jpg", // "pro2
+		title: "Event Management Website",
+		text: "Re-Event is a website that allows the creation of events and the management of the participants.",
+		github_url: "https://github.com/Zediogo96/RE-Event",
+	},
+	{
+		imgsrc: "projects/proj3.jpg", // "pro3
+		title: "Duck Hunt Game in Minix",
+		text: "Clone of the classic game Duck Hunt, developed in C for the Minix operating system.",
+		github_url: "https://github.com/Zediogo96/LCOM_Duck_Hunter",
+	},
+	{
+		imgsrc: "projects/proj4.jpg", // "pro4
+		title: "Supaplex Game Clone",
+		text: "Supaplex is a puzzle game developed in Java, using Lanterna.",
+		github_url: "https://github.com/Zediogo96/Supapl0x-game",
+	},
+];
+
+export default ProjectsData;
